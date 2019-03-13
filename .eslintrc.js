bbad66456e7f2fc2ee5d00d2253cb69258e0be40
Moses Boone <moses.s.boone@gmail.com>
2019-03-12 23:44:49 -0700

    Added eslint rule config, router

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true,
+        'mongo': true        
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'warn'
+        ]
+    }
+};
